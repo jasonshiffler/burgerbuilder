aa4518d211b1ab6a2ad8990347cc9f5f0cf25b17
Jason Shiffler <jasonshiffler@yahoo.com>
2020-07-22 17:57:53 -0500

    Basic burger add/remove ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,14 +3,20 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-  const transformedIngredients = obj2Array(props.ingredients);
+  let transformedIngredients = obj2Array(props.ingredients).map(
+    (ingredient) => {
+      return <BurgerIngredient type={ingredient}></BurgerIngredient>;
+    }
+  );
+
+  if (doIngredientsExist(transformedIngredients) === false) {
+    transformedIngredients = <p>Please start adding ingredients</p>;
+  }
 
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type='bread-top'></BurgerIngredient>
-      {transformedIngredients.map((ingredient) => {
-        return <BurgerIngredient type={ingredient}></BurgerIngredient>;
-      })}
+      {transformedIngredients}
       <BurgerIngredient type='bread-bottom'></BurgerIngredient>
     </div>
   );
@@ -33,3 +39,19 @@ function obj2Array(ingredientObj) {
 }
 
 export default burger;
+
+//Checks to see if we have any ingredients in our list
+function doIngredientsExist(ingredientObj) {
+  const ingredientNumbers = Object.values(ingredientObj);
+  console.log(ingredientNumbers);
+  const sum = ingredientNumbers.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  );
+
+  if (sum === 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
